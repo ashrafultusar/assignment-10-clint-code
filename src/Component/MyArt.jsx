4f504e4fc6e2c116c4ce96c3637519c2e4dc3b01
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-04-28 21:12:26 +0600

    added my craft card delete methode

--- src/Component/MyArt.jsx
+++ src/Component/MyArt.jsx
@@ -6,63 +6,83 @@ import { Link } from "react-router-dom";
 
 const MyArt = () => {
   const { user } = useContext(AuthContex) || {};
-  console.log(user)
+  console.log(user);
   const [item, setItem] = useState();
+  const [controle, setControle] = useState(false);
   console.log(item);
 
-
-
+  // update section
   useEffect(() => {
     if (user) {
       fetch(`http://localhost:5000/myArt/${user?.email}`)
-        .then( (res) => res.json())
+        .then((res) => res.json())
         .then((data) => {
           setItem(data);
         });
     }
-  }, [user]);
+  }, [user,controle]);
 
+  // delet section
+  const handelDelet = (id) => {
+    fetch(`http://localhost:5000/delete/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          setControle(!controle)
+        }
+      });
+  };
 
   return (
     <div>
       <Helmet>
         <title>DREAM ART | My Art</title>
       </Helmet>
-      <h1>total user add card: {user.length}</h1>
-
+      
       <div className="gap-6  grid grid-cols-1 md:grid-cols-3">
-        {
-          item?.map(p => (
+        {item?.map((p) => (
           <div key={p._id}>
             <div>
               <div className="card h-[420px] card-compact  bg-base-100 shadow-xl">
                 <figure>
-                  <img
-                    src={ p.photo}
-                    alt="img"
-                  />
+                  <img src={p.photo} alt="img" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title text-2xl font-bold">{p.itemName}</h2>
-                    <div className="flex justify-between">
-                      <p className="font-semibold text-xl">Price: { p.price}</p>
-                      <p className="font-semibold text-xl">Rating: { p.rating}</p>
-                    </div>
-                    <div className="flex justify-between gap-3">
-                      <p className="font-semibold text-xl">Customization: { p.customization}</p>
-                      <p className="font-semibold text-xl">Stock_Status: <span >{ p.stockStatus}</span> </p>
-                    </div>
-                  
+                  <h2 className="card-title text-2xl font-bold">
+                    {p.itemName}
+                  </h2>
+                  <div className="flex justify-between">
+                    <p className="font-semibold text-xl">Price: {p.price}</p>
+                    <p className="font-semibold text-xl">Rating: {p.rating}</p>
+                  </div>
+                  <div className="flex justify-between gap-3">
+                    <p className="font-semibold text-xl">
+                      Customization: {p.customization}
+                    </p>
+                    <p className="font-semibold text-xl">
+                      Stock_Status: <span>{p.stockStatus}</span>{" "}
+                    </p>
+                  </div>
+
                   <div className="flex justify-between ">
-                   <Link to={`/updatecraft/${p._id}`}>  <button className="btn bg-green-500  ">Update</button></Link>
-                    <button className=" btn bg-red-500 text-white ">Delete</button>
+                    <Link to={`/updatecraft/${p._id}`}>
+                      {" "}
+                      <button className="btn bg-green-500  ">Update</button>
+                    </Link>
+                    <button
+                      onClick={() => handelDelet(p._id)}
+                      className=" btn bg-red-500 text-white "
+                    >
+                      Delete
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          ))
-        }
+        ))}
       </div>
     </div>
   );
